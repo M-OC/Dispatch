0832c9c06ca81439dcfca6ee8a5389b76e1c40bb
Max O'Connell <moconnell7171@gmail.com>
2016-03-18 09:50:49 -0700

    webpack config

---
+++ Build/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+	wut: 'yes!'
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,39 @@
 var path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const TARGET = process.env.npm_lifecycle_event;
 
-var endpoints = {
-	client: path.join(__dirname, '/Client'),
-	build: path.join(__dirname, '/Build')
+const endpoints = {
+	client: path.join(__dirname, 'Client'),
+	build: path.join(__dirname, 'Build')
 };
+
+const common = {
+	entry: {
+		app: endpoints.build
+	},
+	output: {
+		path: endpoints.client,
+		filename: 'app.js'
+	}
+};
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+  	devServer: {
+  		contentBase: endpoints.client,
+  		historyApiFallback: true,
+  		hot: true,
+  		inline: true,
+  		progress: true,
+  		stats: 'errors-only',
+  		host: process.env.HOST,
+  		port: process.env.PORT
+  	},
+  	plugins: [new webpack.HotModuleReplacementPlugin()]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
