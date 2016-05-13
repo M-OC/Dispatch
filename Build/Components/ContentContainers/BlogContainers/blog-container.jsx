aa516709f9be1ba911c39c4da812d202e001aa43
Max O'Connell <moconnell7171@gmail.com>
2016-05-13 14:23:16 -0700

    Performed styling on blog components and introduced flexbox

--- Build/Components/ContentContainers/BlogContainers/blog-container.jsx
+++ Build/Components/ContentContainers/BlogContainers/blog-container.jsx
@@ -5,27 +5,32 @@ import {map} from 'lodash'
 
 const blogEntries = [{
 	title: 'A fake title1',
-	subtitle: 'A fake subtitle1'
+	subtitle: 'A fake subtitle containing text that might be a little more descriptive.',
+	date: '5/10/1022',
+	img: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Male_gorilla_in_SF_zoo.jpg'
 },{
-	title: 'A fake title1',
-	subtitle: 'A fake subtitle1'
+	title: 'A fake title2',
+	subtitle: 'A fake subtitle containing text that might be a little more descriptive.',
+	date: '10/20/3000',
+	img: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Male_gorilla_in_SF_zoo.jpg'
 },{
-	title: 'A fake title1',
-	subtitle: 'A fake subtitle1'
+	title: 'A fake title3',
+	subtitle: 'A fake subtitle containing text that might be a little more descriptive.',
+	date: '10/20/3000',
+	img: 'https://upload.wikimedia.org/wikipedia/commons/5/50/Male_gorilla_in_SF_zoo.jpg'
 }];
 
 var styles = {
-	backgroundColor: "#ccc",
-	margin: "1em"
+	backgroundColor: "#fbfbfb"
 }
 
 export default class BlogContainer extends React.Component {
 	render () {
 		return (
-		<div>
+		<div style={styles}>
 			{map(blogEntries, function(entry) {
 				return (
-				<BlogListItem title={entry.title} subtitle={entry.subtitle}/>
+				<BlogListItem title={entry.title} subtitle={entry.subtitle} date={entry.date} img={entry.img}/>
 				)
 			})}
 		</div>
--- Build/Components/ContentContainers/BlogContainers/blog-list-item.jsx
+++ Build/Components/ContentContainers/BlogContainers/blog-list-item.jsx
@@ -1,5 +1,33 @@
 import React from 'react'
 
+var styles = {
+	main: {
+		margin: "1em 1em",
+		padding: "0 2em",
+		backgroundColor: "#8cf",
+		display: "flex",
+		flexDirection: "row",
+		flexWrap: "wrap",
+		alignItems: "center",
+		justifyContent: "space-between",
+		alignContent: "space-between"
+	},
+	textContainer: {
+		flexGrow: "2"
+	},
+	text: {
+		margin: ".5em 0"
+	},
+	date: {
+		margin: ".25em 0em",
+		fontWeight: "bold"
+	},
+	img: {
+		maxWidth: "8em",
+		height: "auto"
+	}
+}
+
 export default class BlogListItem extends React.Component {
 	constructor (props) {
 		super(props)
@@ -7,9 +35,19 @@ export default class BlogListItem extends React.Component {
 
 	render () {
 		return (
-			<div>
-				<h1>{this.props.title}</h1>
-				<h3>{this.props.subtitle}</h3>
+			<div style={styles.main}>
+				<img style={styles.img} src={this.props.img}/>
+				<div>
+					<h2 style={styles.text}>
+						{this.props.title}
+					</h2>
+					<h4 style={styles.text}>
+						{this.props.subtitle}
+					</h4>
+				</div>
+				<span style={styles.date}>
+					{this.props.date}
+				</span>
 			</div>
 		)
 	}
