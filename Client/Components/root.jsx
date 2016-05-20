1a3a11a67f22f3e4829b642bb694e2ac071dbdf1
Max O'Connell <moconnell7171@gmail.com>
2016-05-20 11:08:39 -0700

    Changes to spacing.

--- Client/Components/root.jsx
+++ Client/Components/root.jsx
@@ -3,7 +3,6 @@ import NavBar from './NavBar/nav-bar.jsx'
 import MainContainer from './ContentContainers/main-container.jsx'
 
 
-
 export default class Root extends React.Component {
 	render () {
 		return (
--- Client/Store/store.js
+++ Client/Store/store.js
@@ -1,6 +1,8 @@
 import {createStore} from 'redux'
 import Reducers from './Reducers/index.js'
 
+
 var Store = createStore(Reducers);
+
 
 export default Store
